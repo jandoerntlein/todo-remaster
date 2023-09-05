d07b55edfefec61adf6491e782c41367ded71206
Jan Doerntlein <jan@doerntlein.com>
2023-09-05 09:06:06 +0200

    task: use shadcn-Input instead of input; rename clip-rule and fill-rule

--- app/components/todo/todo.tsx
+++ app/components/todo/todo.tsx
@@ -11,6 +11,7 @@ import {
   CardTitle,
 } from "@/app/components/ui/card"
 import Link from "next/link"
+import { Input } from "@/app/components/ui/input"
 
 const InitialTODOs = [
 	{
@@ -28,7 +29,7 @@ const InitialTODOs = [
 ]
 
 const Cross2Icon = () => {
-	return (<svg width="12" height="12" viewBox="0 0 15 15" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path d="M11.7816 4.03157C12.0062 3.80702 12.0062 3.44295 11.7816 3.2184C11.5571 2.99385 11.193 2.99385 10.9685 3.2184L7.50005 6.68682L4.03164 3.2184C3.80708 2.99385 3.44301 2.99385 3.21846 3.2184C2.99391 3.44295 2.99391 3.80702 3.21846 4.03157L6.68688 7.49999L3.21846 10.9684C2.99391 11.193 2.99391 11.557 3.21846 11.7816C3.44301 12.0061 3.80708 12.0061 4.03164 11.7816L7.50005 8.31316L10.9685 11.7816C11.193 12.0061 11.5571 12.0061 11.7816 11.7816C12.0062 11.557 12.0062 11.193 11.7816 10.9684L8.31322 7.49999L11.7816 4.03157Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path></svg>)
+	return (<svg width="12" height="12" viewBox="0 0 15 15" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path d="M11.7816 4.03157C12.0062 3.80702 12.0062 3.44295 11.7816 3.2184C11.5571 2.99385 11.193 2.99385 10.9685 3.2184L7.50005 6.68682L4.03164 3.2184C3.80708 2.99385 3.44301 2.99385 3.21846 3.2184C2.99391 3.44295 2.99391 3.80702 3.21846 4.03157L6.68688 7.49999L3.21846 10.9684C2.99391 11.193 2.99391 11.557 3.21846 11.7816C3.44301 12.0061 3.80708 12.0061 4.03164 11.7816L7.50005 8.31316L10.9685 11.7816C11.193 12.0061 11.5571 12.0061 11.7816 11.7816C12.0062 11.557 12.0062 11.193 11.7816 10.9684L8.31322 7.49999L11.7816 4.03157Z" fill="currentColor" fillRule="evenodd" clipRule="evenodd"></path></svg>)
 }
 
 export default function Todo() {
@@ -111,7 +112,7 @@ export default function Todo() {
 								{todo.text}			
 							</label>
 							</div>
-							<div class="flex flex-col justify-end items-start">
+							<div className="flex flex-col justify-end items-start">
 								<Link onClick={event=>removeTODO(event, index)} href="#"><Cross2Icon/></Link>
 							</div>
 						</div>
@@ -121,7 +122,7 @@ export default function Todo() {
 		</div>
 		</CardContent>
 		<CardFooter>
-		  <input type="text" placeholder="Add new TODO item..." onKeyDown={event=>addTODO(event)}  style={{width: "100%"}}></input>
+		  <Input type="text" placeholder="Add new TODO item..." onKeyDown={event=>addTODO(event)}  style={{width: "100%"}}></Input>
 		</CardFooter>
 	  </Card>
 	)
